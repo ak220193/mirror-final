c44695b6b0c3af61e90e1f69e786cf58a2a783b0
ak220193 <mail2meak22frcrio@gmail.com>
2024-05-20 00:18:05 +0530

    navabr changes

--- src/components/Homepage/Navbar.jsx
+++ src/components/Homepage/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import Logo from "../../assets/mirrorlogo.jpg";
 import { IoCloseSharp } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="absolute top-0 left-0 right-0 p-2 flex items-center justify-between z-10">
+    <nav className="absolute top-0 left-0 right-0 p-2 flex items-center justify-between  z-10">
       {/* Logo for small and medium screens */}
       <div className="md:hidden">
         <img src={Logo} alt="Logo" className="h-8" />
@@ -29,55 +29,28 @@ const Navbar = () => {
       {/* Fullscreen menu for small and medium screens */}
       {isOpen && (
         <div className="fixed inset-0 bg-white z-20 flex flex-col items-center justify-center space-y-4">
-          <button
-            onClick={toggleMenu}
-            className="absolute top-4 right-4 text-black focus:outline-none"
-          >
+          <button onClick={toggleMenu} className="absolute top-4 right-4 text-black focus:outline-none">
             <IoCloseSharp className="w-8 h-8" />
           </button>
-          <a href="/" className="text-black hover:text-gray-600">
-            Home
-          </a>
-          <a href="/about" className="text-black hover:text-gray-600">
-            About
-          </a>
-          <a href="/services" className="text-black hover:text-gray-600">
-            Services
-          </a>
-          <a href="/portfolio" className="text-black hover:text-gray-600">
-            Portfolio
-          </a>
-          <a href="/getinked" className="text-black hover:text-gray-600">
-            Get Inked
-          </a>
-          <a href="/aftercare" className="text-black hover:text-gray-600">
-            After Care
-          </a>
+          <a href="/" className="text-black hover:text-red-600">Home</a>
+          <a href="/about" className="text-black hover:text-red-600">About</a>
+          <a href="/services" className="text-black hover:text-red-600">Services</a>
+          <a href="/portfolio" className="text-black hover:text-red-600">Portfolio</a>
+          <a href="/getinked" className="text-black hover:text-red-600">Get Inked</a>
+          <a href="/aftercare" className="text-black hover:text-red-600">After Care</a>
         </div>
       )}
       {/* Navbar for large screens */}
-      <div className="hidden  md:flex w-full space-x-8 items-center justify-around text-white">
-        <a href="/" className="hover:text-gray-300">
-          Home
-        </a>
-        <a href="/about" className="hover:text-gray-300">
-          About
-        </a>
-        <a href="/services" className="hover:text-gray-300">
-          Services
-        </a>
+      <div className="hidden gap-2 md:flex w-full items-center justify-around text-white lg:flex m-4">
+        <a href="/" className="hover:text-red-600">Home</a>
+        <a href="/about" className="hover:text-red-600">About</a>
+        <a href="/services" className="hover:text-red-600">Services</a>
         <a href="/" className="flex items-center">
           <img src={Logo} alt="Logo" className="h-10 mx-5" />
         </a>
-        <a href="/portfolio" className="hover:text-gray-300">
-          Portfolio
-        </a>
-        <a href="/getinked" className="hover:text-gray-300">
-          Get Inked
-        </a>
-        <a href="/aftercare" className="hover:text-gray-300">
-          After Care
-        </a>
+        <a href="/portfolio" className="hover:text-red-600">Portfolio</a>
+        <a href="/getinked" className="hover:text-red-600">Get Inked</a>
+        <a href="/aftercare" className="hover:text-red-600">After Care</a>
       </div>
     </nav>
   );
