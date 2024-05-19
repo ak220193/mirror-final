4c2a882dc0291a80f19b25f5a3520472a101c2fa
ak220193 <mail2meak22frcrio@gmail.com>
2024-05-19 21:18:56 +0530

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AppRouter from './router/AppRouter';
 
 function App() {
   return (
     <>
-      
+    <AppRouter />
     </>
   )
 }
---
+++ src/pages/aftercare.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const aftercare = () => {
+  return (
+    <div>aftercare</div>
+  )
+}
+
+export default aftercare
