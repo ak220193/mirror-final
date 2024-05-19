f4295384fc9907e92f236816c7b76ebebc7158e6
ak220193 <mail2meak22frcrio@gmail.com>
2024-05-19 18:58:47 +0530

    project intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+        'primary': ['Raleway', 'sans-serif'],
+        // 'algerian': ['Algerian', 'serif'],
+        // 'montestrat': ['montestrat']
+      },
+      container: {
+        padding: {
+          DEFAULT: '20px',
+          lg: '0',
+        },
+      },
+      
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1300px',
+      },
+
+      colors: {
+        primary: '#c71212',
+        secondary: '#fa9397',
+        dark: '#333333'
+      },
+      fontSize: {
+        '2xs': '.680rem', // Your custom font size
+        '3xs': '.550rem',
+      },
+    },
+  },
+  plugins: [],
+}
